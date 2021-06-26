9b92573fe6ad0042299819245b69a8c07e3aa79a
Ultraman-Mark <2278625687@qq.com>
2021-06-26 17:58:03 +0800

    version 1

--- src/main/java/com/RPC/Client/RPCClient.java
+++ src/main/java/com/RPC/Client/RPCClient.java
@@ -1,30 +1,19 @@
 package com.RPC.Client;
 
 import com.RPC.Pojo.User;
-
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.Socket;
-import java.util.Random;
+import com.RPC.Server.UserService;
 
 public class RPCClient {
     public static void main(String[] args) {
-        try{
-            //建立Socket连接
-            Socket socket = new Socket("127.0.0.1",8899);
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
-            ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
-            //传递给服务器id
-            objectOutputStream.writeInt(new Random().nextInt());
-            objectOutputStream.flush();
-            //服务器查询数据，返回对应的对象
-            User user = (User) objectInputStream.readObject();
-            System.out.println("服务端返回User:"+user);
-        }
-        catch (IOException | ClassCastException | ClassNotFoundException e){
-            e.printStackTrace();
-            System.out.println("客户端启动失败");
-        }
+        ClientProxy clientProxy = new ClientProxy("127.0.0.1",8899);
+        UserService proxy = clientProxy.getProxy(UserService.class);
+
+        //服务方法 1
+        User userByUserId = proxy.getUserByUserId(10);
+        System.out.println("从服务端得到的user为："+userByUserId);
+        //服务方法 2
+        User user = User.builder().userName("张三").id(100).sex(true).build();
+        Integer integer = proxy.insertUserId(user);
+        System.out.println("从服务端插入数据："+integer);
     }
 }
--- src/main/java/com/RPC/Server/RPCServer.java
+++ src/main/java/com/RPC/Server/RPCServer.java
@@ -1,11 +1,14 @@
 package com.RPC.Server;
 
-import com.RPC.Pojo.User;
+import com.RPC.Client.RPCRequest;
+import com.RPC.Client.RPCResponse;
 import com.RPC.Server.Impl.UserServiceImpl;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -15,20 +18,24 @@ public class RPCServer {
         try {
             ServerSocket serverSocket = new ServerSocket(8899);
             System.out.println("服务端启动");
+            // BIO的方式监听Socket
             while (true){
                 Socket socket = serverSocket.accept();
+                // 开启一个线程去处理
                 new Thread(()->{
                     try{
                         ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
                         ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-                        //读取客户端ID
-                        Integer id = ois.readInt();
-                        User userByUserId = userService.getUserByUserId(id);
-                        //写入User对象给客户端
-                        oos.writeObject(userByUserId);
+                        // 读取客户端传过来的request
+                        RPCRequest request = (RPCRequest) ois.readObject();
+                        // 反射调用对应方法
+                        Method method = userService.getClass().getMethod(request.getMethodName(),request.getParamsTypes());
+                        Object invoke = method.invoke(userService,request.getParams());
+                        // 封装，写入response对象
+                        oos.writeObject(RPCResponse.success(invoke));
                         oos.flush();
                     }
-                    catch (IOException e){
+                    catch (IOException | ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e){
                         e.printStackTrace();
                         System.out.println("从IO中读取数据错误");
                     }
